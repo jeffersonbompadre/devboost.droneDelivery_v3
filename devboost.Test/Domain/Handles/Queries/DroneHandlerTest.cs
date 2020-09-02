be28e30ed67c2cd3f5b03c979d34a7ec6e1428aa
Allan Freitas <freitas_allan@hotmail.com>
2020-09-02 19:56:29 -0300

    DroneHandlerTest.cs

--- devboost.Test/Domain/Handles/Queries/DroneHandlerTest.cs
+++ devboost.Test/Domain/Handles/Queries/DroneHandlerTest.cs
@@ -1,7 +1,32 @@
-﻿namespace devboost.Test.Domain.Handles.Queries
+﻿using devboost.Domain.Handles.Queries.Interfaces;
+using devboost.Domain.Model;
+using devboost.Test.Config;
+using devboost.Test.Warmup;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devboost.Test.Domain.Handles.Queries
 {
     public class DroneHandlerTest
     {
+        IDroneHandler _droneHandler;
+        readonly IDataStart _dataStart;
 
+        public DroneHandlerTest()
+        {
+            _droneHandler = StartInjection.GetServiceCollection().GetService<IDroneHandler>();
+            _dataStart = StartInjection.GetServiceCollection().GetService<IDataStart>();
+            // Popula base de dados
+            _dataStart.Seed();
+        }
+
+        [Fact]
+        public async Task BuscarDrone()
+        {
+            List<Drone> lista = await _droneHandler.BuscarDrone();
+            Assert.True(lista.Count > 0);
+        }
     }
 }
