1cefc00ca10b821452a3797ea9699ace61ef7e0c
Eric Joseph Bezerra Costa <frodoeric@hotmail.com>
2020-09-01 21:58:42 -0300

    inclusão método Drone

---
+++ devboost.Test/Repository/DroneRepositoryTest.cs
@@ -0,0 +1,36 @@
+﻿using devboost.Domain.Model;
+using devboost.Domain.Repository;
+using devboost.Test.Config;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace devboost.Test.Repository
+{
+    public class DroneRepositoryTest
+    {
+        readonly IDroneRepository _droneRepository;
+
+        public DroneRepositoryTest()
+        {
+            _droneRepository = StartInjection.GetServiceCollection().GetService<IDroneRepository>();
+        }
+
+        [Fact]
+        public async Task TestaRetornoDeTodosDrone()
+        {
+            var cliResult = await _droneRepository.GetAll();
+            Assert.NotNull(cliResult);
+        }
+
+        [Fact]
+        public async Task TestaConsultaDroneDisponivel()
+        {
+            var cliResult = await _droneRepository.GetDronesDisponiveis();
+            Assert.NotNull(cliResult);
+        }
+    }
+}
