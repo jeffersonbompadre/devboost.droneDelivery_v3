2bea975fbfa5620c32705d2513e38134cdb70a8a
jefferson.bompadre <jefferson.bompadre@smlbrasil.com.br>
2020-09-02 19:50:30 -0300

    Add test: Domain e CommandHandler

---
+++ devboost.Test/Domain/Handles/ClienteHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using devboost.Domain.Commands.Request;
+using devboost.Domain.Handles.Commands.Interfaces;
+using devboost.Test.Config;
+using devboost.Test.Warmup;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace devboost.Test.Domain.Handles
+{
+    public class ClienteHandlerTest
+    {
+        readonly IClienteHandler _clientHandler;
+        readonly IDataStart _dataStart;
+
+        public ClienteHandlerTest()
+        {
+            _clientHandler = StartInjection.GetServiceCollection().GetService<IClienteHandler>();
+            _dataStart = StartInjection.GetServiceCollection().GetService<IDataStart>();
+            // Popula base de dados
+            _dataStart.Seed();
+        }
+
+        [Theory]
+        [InlineData("João da Silva", "joao.silva@domain.com", "(11) 9999-9999", -23.5990684, -46.6784195, "Jefferson")]
+        public async void TestaAddCliente(string nome, string email, string telefone, double latitude, double longitude, string usuario)
+        {
+            await _clientHandler.AddCliente(new ClienteRequest()
+            {
+                Nome = nome,
+                EMail = email,
+                Telefone = telefone,
+                Latitude = latitude,
+                Longitude = longitude
+            }, usuario);
+        }
+    }
+}
---
+++ devboost.Test/Domain/Handles/PedidoHandlerTest.cs
@@ -0,0 +1,38 @@
+﻿using devboost.Domain.Commands.Request;
+using devboost.Domain.Handles.Commands.Interfaces;
+using devboost.Test.Config;
+using devboost.Test.Warmup;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+
+namespace devboost.Test.Domain.Handles
+{
+    public class PedidoHandlerTest
+    {
+        readonly IPedidoHandler _pedidoHandler;
+        readonly IDataStart _dataStart;
+
+        public PedidoHandlerTest()
+        {
+            _pedidoHandler = StartInjection.GetServiceCollection().GetService<IPedidoHandler>();
+            _dataStart = StartInjection.GetServiceCollection().GetService<IDataStart>();
+            // Popula base de dados
+            _dataStart.Seed();
+        }
+
+        [Theory]
+        [InlineData(5, "Erick")]
+        public async void TestaRealizarPedido(int peso, string usuario)
+        {
+            var pedido = await _pedidoHandler.RealizarPedido(new RealizarPedidoRequest { Peso = peso }, usuario );
+            Assert.NotNull(pedido);
+        }
+
+        [Fact]
+        public async void TestaDistribuirPedido()
+        {
+            await _pedidoHandler.DistribuirPedido();
+        }
+    }
+}
