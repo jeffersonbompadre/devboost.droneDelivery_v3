675060c240b7d80662d998ce2e993b41b5736ec6
jefferson.bompadre <jefferson.bompadre@smlbrasil.com.br>
2020-09-02 20:45:22 -0300

    Add Authentication.feature

---
+++ devboost.SpecFlowTest/Features/Authentication.feature
@@ -0,0 +1,7 @@
+﻿Feature: Realizar Login para Autenticar no sistema
+
+Scenario: Realizar o login
+	Given Que exista um usuário cadastrado na base
+	When Quando for informado o login
+	Then Será retornado um token
+	And será retornado um status code 200
---
+++ devboost.SpecFlowTest/Steps/RealizarLoginParaAutenticarNoSistemaSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace devboost.SpecFlowTest.Steps
+{
+    [Binding]
+    public class RealizarLoginParaAutenticarNoSistemaSteps
+    {
+        readonly ScenarioContext _context;
+
+        public RealizarLoginParaAutenticarNoSistemaSteps(ScenarioContext context)
+        {
+            _context = context;
+        }
+
+        [Given(@"Que exista um usuário cadastrado na base")]
+        public void GivenQueExistaUmUsuarioCadastradoNaBase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Quando for informado o login")]
+        public void WhenQuandoForInformadoOLogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Será retornado um token")]
+        public void ThenSeraRetornadoUmToken()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"será retornado um status code (.*)")]
+        public void ThenSeraRetornadoUmStatusCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
