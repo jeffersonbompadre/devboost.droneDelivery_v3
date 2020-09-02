931810c65b4f3a29a8bd508b8d5deca3e81f5cb3
Allan Freitas <freitas_allan@hotmail.com>
2020-09-02 08:45:06 -0300

    PedidoRepositoryTest.cs AddPedido

--- devboost.Test/Repository/PedidoRepositoryTest.cs
+++ devboost.Test/Repository/PedidoRepositoryTest.cs
@@ -3,6 +3,7 @@ using devboost.Domain.Repository;
 using devboost.Test.Config;
 using devboost.Test.Warmup;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -24,8 +25,30 @@ namespace devboost.Test.Repository
         [Fact]
         public async Task GetPedidos()
         {
-            List<Pedido> lista = await _pedidoRepository.GetPedidos(StatusPedido.aguardandoAprovacao);
-            Assert.NotNull(lista);
+            List<Pedido> lista = await _pedidoRepository.GetPedidos(StatusPedido.aguardandoEntrega);
+            Assert.True(lista.Count > 0);
+        }
+
+        [Fact]
+        public async Task GetPedidosPorStatusPesoDistancia()
+        {
+            List<Pedido> listaPedidos = await _pedidoRepository.GetPedidos(StatusPedido.aguardandoEntrega, 2, 2);
+            Assert.True(listaPedidos.Count > 0);
+        }
+
+        [Fact]
+        public async Task AddPedido()
+        {
+            await _pedidoRepository.AddPedido(new Pedido 
+            { Id = Guid.NewGuid(),
+                Peso = 4,
+                DataHora = DateTime.Now,
+                DistanciaParaOrigem = 2,
+                StatusPedido = StatusPedido.despachado
+            });
+
+            List<Pedido> lista = await _pedidoRepository.GetPedidos(StatusPedido.despachado);
+            Assert.True(lista.Count > 0);
         }
     }
 }
