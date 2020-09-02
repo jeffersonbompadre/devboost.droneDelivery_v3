3e4e005d21e1187704a1c1f3d238c1f666e78264
jefferson.bompadre <jefferson.bompadre@smlbrasil.com.br>
2020-09-02 20:00:35 -0300

    Add teste de login

---
+++ devboost.Test/Domain/Handles/Queries/LoginHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using devboost.Domain.Handles.Queries.Interfaces;
+using devboost.Domain.Queries.Filters;
+using devboost.Test.Config;
+using devboost.Test.Warmup;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace devboost.Test.Domain.Handles.Queries
+{
+    public class LoginHandlerTest
+    {
+        readonly ILoginHandler _loginHandler;
+        readonly IDataStart _dataStart;
+
+        public LoginHandlerTest()
+        {
+            _loginHandler = StartInjection.GetServiceCollection().GetService<ILoginHandler>();
+            _dataStart = StartInjection.GetServiceCollection().GetService<IDataStart>();
+            // Popula base de dados
+            _dataStart.Seed();
+        }
+
+        [Theory]
+        [InlineData("Erick", "12345")]
+        public async void TestaLoginUser(string userName, string password)
+        {
+            var login = await _loginHandler.LoginUser(new QueryUserFilter { UserName = userName, Password = password });
+            Assert.NotNull(login);
+        }
+    }
+}
